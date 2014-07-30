6914e7cc620abf0857cc5e407d0e04e6b016ec3e
Pedro Figuera <pedroafiguera@gmail.com>
2014-07-30 15:06:26 -0400

    Installed active admin and ran migrations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ StripePayments::Application.configure do
   config.action_mailer.raise_delivery_errors = false
   # dev mode- send emails
   config.action_mailer.delivery_method = :letter_opener
-
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
