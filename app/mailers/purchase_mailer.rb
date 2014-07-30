aa7cf904fb141289e58d11ea23a43ff6d7150e95
Pedro Figuera <pedroafiguera@gmail.com>
2014-07-30 14:41:41 -0400

    Adding emailing of a purchase receipt

---
+++ app/mailers/purchase_mailer.rb
@@ -0,0 +1,10 @@
+class PurchaseMailer < ActionMailer::Base
+ layout 'purchase_mailer'
+ default from: "One Month Commerce <commerce@onemonth.com>"
+
+ def purchase_receipt purchase
+   @purchase = purchase
+   mail to: purchase.email, subject: "Thanks for your purchase!"
+
+ end
+end
